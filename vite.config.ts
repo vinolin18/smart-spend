f6587a2a34bc927ffb907d1c58b5f3a64ecc9bdc
Vinolin Deborah Delphin <vinolin18@gmail.com>
2025-12-19 23:44:52 +0530

    Configure for Vite and GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/smart-spend/', // REPLACE 'smart-spend' with your repo name
+  base: '/smart-spend/', 
 });
